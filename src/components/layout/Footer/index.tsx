003d9930f1251a83917e41a58636e5cbae14f835
Denis Klimov <plitnichenko@gmail.com>
2021-12-04 01:52:22 +0300

    DexAda re-design additional fixes

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,51 +1,15 @@
 import * as React from 'react'
-import { Observer } from 'mobx-react-lite'
 import { useIntl } from 'react-intl'
 import { Link, NavLink } from 'react-router-dom'
 
 import { Icon } from '@/components/common/Icon'
 import { Logo } from '@/components/layout/Logo'
-import { useWallet } from '@/stores/WalletService'
 
 import './index.scss'
 
 
 export function Footer(): JSX.Element {
     const intl = useIntl()
-    const wallet = useWallet()
-
-    const toolbar = (
-        <div className="toolbar">
-            <Observer>
-                {() => (
-                    <>
-                        {(!wallet.isInitialized && !wallet.isInitializing) && (
-                            <a
-                                href="https://chrome.google.com/webstore/detail/ton-crystal-wallet/cgeeodpfagjceefieflmdfphplkenlfk"
-                                className="btn btn--xl btn--tertiary footer-tool"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                {intl.formatMessage({
-                                    id: 'WALLET_INSTALLATION_LINK_TEXT',
-                                })}
-                            </a>
-                        )}
-                    </>
-                )}
-            </Observer>
-            <a
-                href="https://github.com/broxus/ton-dex"
-                className="btn btn--tertiary footer-tool"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                {intl.formatMessage({
-                    id: 'FOOTER_GITHUB_LINK_TEXT',
-                })}
-            </a>
-        </div>
-    )
 
     return (
         <footer className="footer">
@@ -55,7 +19,6 @@ export function Footer(): JSX.Element {
                         <Link to="/" className="footer-logo">
                             <Logo />
                         </Link>
-                        {toolbar}
                     </div>
                     <nav className="footer-nav">
                         <div className="footer-nav__col">
@@ -102,57 +65,13 @@ export function Footer(): JSX.Element {
                                 </li>
                             </ul>
                         </div>
-                        <div className="footer-nav__col">
-                            <div className="footer-nav__col-title">
-                                {intl.formatMessage({
-                                    id: 'FOOTER_NAV_HEADER_DEFI_PRODUCTS',
-                                })}
-                            </div>
-                            <ul className="footer-nav__list">
-                                <li>
-                                    <a href="https://tonbridge.io" target="_blank" rel="noopener noreferrer">
-                                        {intl.formatMessage({
-                                            id: 'FOOTER_NAV_TON_BRIDGE_LINK_TEXT',
-                                        })}
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://tonscan.io" target="_blank" rel="noopener noreferrer">
-                                        {intl.formatMessage({
-                                            id: 'FOOTER_NAV_TON_SCAN_LINK_TEXT',
-                                        })}
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="https://wton.io" target="_blank" rel="noopener noreferrer">
-                                        {intl.formatMessage({
-                                            id: 'FOOTER_NAV_WTON_LINK_TEXT',
-                                        })}
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        href="https://chrome.google.com/webstore/detail/ton-crystal-wallet/cgeeodpfagjceefieflmdfphplkenlfk"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        {intl.formatMessage({
-                                            id: 'FOOTER_NAV_CRYSTAL_WALLET_LINK_TEXT',
-                                        })}
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
                     </nav>
-                    <div className="footer__right">
-                        {toolbar}
-                    </div>
                 </div>
                 <div className="footer__bottom">
                     <ul className="footer-soc">
                         <li>
                             <a
-                                href="https://t.me/tonbridge_official"
+                                href="https://t.me/CardanoAnnouncements"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 title="Telegram"
@@ -162,7 +81,7 @@ export function Footer(): JSX.Element {
                         </li>
                         <li>
                             <a
-                                href="https://twitter.com/Broxus"
+                                href="https://twitter.com/Cardano"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 title="Twitter"
@@ -172,7 +91,7 @@ export function Footer(): JSX.Element {
                         </li>
                         <li>
                             <a
-                                href="https://github.com/broxus"
+                                href="https://github.com/cardano-foundation"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 title="GitHub"
@@ -182,7 +101,7 @@ export function Footer(): JSX.Element {
                         </li>
                         <li>
                             <a
-                                href="https://broxus.medium.com/"
+                                href="https://medium.com/@cardano.foundation"
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 title="Medium"
@@ -208,7 +127,7 @@ export function Footer(): JSX.Element {
                             <ul>
                                 <li>
                                     <a
-                                        href="https://broxus.com/wp-content/uploads/2021/08/terms_of_use.pdf"
+                                        href="https://cardanofoundation.org/en/terms-and-conditions/"
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >
@@ -219,7 +138,7 @@ export function Footer(): JSX.Element {
                                 </li>
                                 <li>
                                     <a
-                                        href="https://broxus.com/wp-content/uploads/2021/08/privacy_policy.pdf"
+                                        href="https://cardanofoundation.org/en/privacy/"
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >
@@ -228,6 +147,7 @@ export function Footer(): JSX.Element {
                                         })}
                                     </a>
                                 </li>
+                                {/*
                                 <li>
                                     <a
                                         href="https://broxus.com/wp-content/uploads/2021/08/cookie_policy.pdf"
@@ -239,6 +159,7 @@ export function Footer(): JSX.Element {
                                         })}
                                     </a>
                                 </li>
+                                */}
                             </ul>
                         </nav>
                     </div>
