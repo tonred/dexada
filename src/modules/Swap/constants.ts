11148d6bb56dbd494fb9d6329757f49a4a022cdc
pavel <pavelkhachatrian@gmail.com>
2021-12-03 14:51:01 +0200

    change default swap tokens

--- src/modules/Swap/constants.ts
+++ src/modules/Swap/constants.ts
@@ -7,11 +7,11 @@ import {
 } from '@/modules/Swap/types'
 
 
-/* WTON root address */
-export const DEFAULT_LEFT_TOKEN_ROOT = '0:0ee39330eddb680ce731cd6a443c71d9069db06d149a9bec9569d1eb8d04eb37'
+/* ADA root address */
+export const DEFAULT_LEFT_TOKEN_ROOT = '0:2bac5f96685be79a9d8cac73e54803310f6f15f34dce5361563333bf432deb65'
 
-/* USDT root address */
-export const DEFAULT_RIGHT_TOKEN_ROOT = '0:751b6e22687891bdc1706c8d91bf77281237f7453d27dc3106c640ec165a2abf'
+/* WTON root address */
+export const DEFAULT_RIGHT_TOKEN_ROOT = '0:0ee39330eddb680ce731cd6a443c71d9069db06d149a9bec9569d1eb8d04eb37'
 
 export const DEFAULT_DECIMALS = 18
 
