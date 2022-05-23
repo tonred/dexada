d8aa1f8b1afcf2edc5fc764d532a86cbed980340
pavelkhachatrian <pavelkhachatrian@gmail.com>
2022-05-24 00:05:10 +0700

    update api endpoints

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'https://ton-swap-indexer-test.broxus.com/v1'
-export const FARMING_POOL_API_URL = 'https://farming-pool-indexer.broxus.com/v1'
+export const API_URL = 'https://api.flatqube.io/v1'
+export const FARMING_POOL_API_URL = 'https://farming.flatqube.io/v1'
 
 export const SECONDS_IN_DAY = 86400
